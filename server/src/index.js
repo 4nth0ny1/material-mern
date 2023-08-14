dc2f2d89ab0ee382af8eae6523d684760759b321
4nth0ny1 <acatullo4@gmail.com>
2023-08-14 16:42:16 -0500

    added cors and mongo

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const cors = require("cors");
 
 dotenv.config();
 console.log(process.env.MONGO_URI);
 
 const app = express();
 
+app.use(express.json());
+app.use(cors());
 app.use(morgan("tiny"));
 
 app.get("/todos", (req, res) => {
