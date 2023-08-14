1ffcd92111fda6d81d9b06a51819aa755c49743d
4nth0ny1 <acatullo4@gmail.com>
2023-08-14 18:31:06 -0500

    added creatTodoRoute

--- server/src/router.js
+++ server/src/router.js
@@ -5,8 +5,10 @@ const router = express.Router();
 
 const loginRoute = require("./routes/loginRoute");
 const getTodosRoute = require("./routes/getTodosRoute");
+const createTodoRoute = require("./routes/createTodoRoute");
 
 router.post("/login", loginRoute);
 router.get("/todos", isLoggedIn, getTodosRoute);
+router.post("/todos", isLoggedIn, createTodoRoute);
 
 module.exports = router;
